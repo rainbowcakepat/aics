4993464eb9ab353d57432038021053a46c78010e
kaellsnd <60645855+kaellsnd@users.noreply.github.com>
2021-08-21 12:51:25 +0800

    integrate firebase for Login function

---
+++ AICSFINAL/myapp/firebase.js
@@ -0,0 +1,24 @@
+import * as firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyDZLwlX8IBlv7e5l1KfbF3hwkOMNZpekvc',
+  authDomain: 'aics-chatbot.firebaseapp.com',
+  projectId: 'aics-chatbot',
+  storageBucket: 'aics-chatbot.appspot.com',
+  messagingSenderId: '98998431128',
+  appId: '1:98998431128:web:16711d6db77a60eb324fed',
+  measurementId: 'G-3N9WHRSNBR',
+};
+
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+export {db, auth};
--- AICSFINAL/myapp/screens/loginF.js
+++ AICSFINAL/myapp/screens/loginF.js
@@ -1,9 +1,23 @@
-import React, {useState} from 'react';
-import { TextInput, TouchableHighlight, Linking, Dimensions, Image, StyleSheet, Text, TouchableOpacity, View,} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  TextInput,
+  TouchableHighlight,
+  Linking,
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import { heightPercentageToDP as hp, widthPercentageToDP as wp, } from 'react-native-responsive-screen'
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 
 import {globalStyles} from '../styles/global';
+import {auth} from '../firebase';
 
 const screenwidth = Dimensions.get('window').width;
 const screenheight = Dimensions.get('window').height;
@@ -11,83 +25,126 @@ const screenheight = Dimensions.get('window').height;
 const win = Dimensions.get('window');
 
 const Login = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signin = () => {
+    auth.signInWithEmailAndPassword(email, password).catch(error => {
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(function (user) {
+      if (user) {
+        //User is signed in
+        navigation.navigate('AkishaChatbot');
+      } else {
+        //No user is signed in
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
-    
-  <View style={globalStyles.wholePageContainer} >
-        
-          <LinearGradient style={globalStyles.pageHeaderContainer} colors= {['#363636', '#232526', '#1F1C18']}>
-          
-          <View style={globalStyles.pageHeaderElements}>
-            <Image  style={globalStyles.ust} source={require('../assets/ust.png')}/>
-            <Image  style={globalStyles.ustiics} source={require('../assets/ustiics.png')}/>  
-          </View>
+    <View style={globalStyles.wholePageContainer}>
+      <LinearGradient
+        style={globalStyles.pageHeaderContainer}
+        colors={['#363636', '#232526', '#1F1C18']}>
+        <View style={globalStyles.pageHeaderElements}>
+          <Image
+            style={globalStyles.ust}
+            source={require('../assets/ust.png')}
+          />
+          <Image
+            style={globalStyles.ustiics}
+            source={require('../assets/ustiics.png')}
+          />
+        </View>
 
-          <View style={globalStyles.pageHeaderTexts}>
-            <Text style={globalStyles.ustText}>University of Santo Tomas</Text>
-            <Text style={globalStyles.ustiicsText}>College of Information and Computing Sciences</Text>
-          </View>
+        <View style={globalStyles.pageHeaderTexts}>
+          <Text style={globalStyles.ustText}>University of Santo Tomas</Text>
+          <Text style={globalStyles.ustiicsText}>
+            College of Information and Computing Sciences
+          </Text>
+        </View>
+      </LinearGradient>
 
-        </LinearGradient>
+      <LinearGradient
+        style={globalStyles.pageBodyContainer}
+        colors={['white', 'white', 'white', 'maroon']}>
+        <Image
+          style={globalStyles.loadingBg}
+          source={require('../assets/loginbg.png')}
+        />
 
-        <LinearGradient style={globalStyles.pageBodyContainer} colors= {['white', 'white','white', 'maroon']}>
-          
-          <Image  style={globalStyles.loadingBg} source={require('../assets/loginbg.png')}/>
-            
-          <View style={globalStyles.credentialsBg}></View>
-            <View style={globalStyles.credentials}>
-                    <Text style={styles.welcome}>Welcome, Admin!</Text>
-                    <Text style={styles.unameText}>Username:</Text>
-                    <TextInput style={styles.uname} placeholder='Enter your username' type='email'></TextInput>
-                    <Text style={styles.passText}>Password:</Text>
-                    <TextInput style={styles.pass} placeholder='Enter your password' secureTextEntry></TextInput>
-                    <TouchableOpacity style={styles.login}>
-                      <Text style={styles.loginText}>Login</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                      <Text style={styles.forgotPasswordText} onPress={() => Linking.openURL('https://console.firebase.google.com/u/0/')}>Forgot Password?</Text>
-                    </TouchableOpacity>
-            </View>
+        <View style={globalStyles.credentialsBg}></View>
+        <View style={globalStyles.credentials}>
+          <Text style={styles.welcome}>Welcome, Admin!</Text>
+          <Text style={styles.unameText}>Email:</Text>
+          <TextInput
+            style={styles.uname}
+            placeholder="Enter your username"
+            type="email"
+            value={email}
+            onChangeText={text => setEmail(text)}></TextInput>
+          <Text style={styles.passText}>Password:</Text>
+          <TextInput
+            style={styles.pass}
+            placeholder="Enter your password"
+            secureTextEntry
+            value={password}
+            onChangeText={text => setPassword(text)}></TextInput>
+          <TouchableOpacity style={styles.login} onPress={signin}>
+            <Text style={styles.loginText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text
+              style={styles.forgotPasswordText}
+              onPress={() =>
+                Linking.openURL('https://console.firebase.google.com/u/0/')
+              }>
+              Forgot Password?
+            </Text>
+          </TouchableOpacity>
+        </View>
 
-            {/* <View style={styles.avatars}>
+        {/* <View style={styles.avatars}>
               <Image  style={styles.iicsboy} source={require('../assets/iicsboy.png')}/>
               <Image  style={styles.akisha} source={require('../assets/akisha.png')}/>
             </View> */}
 
-
-         {/* <View style={styles.iconsContainer}>
+        {/* <View style={styles.iconsContainer}>
           <Image  style={styles.iicsboy} source={require('../assets/iicsboy.png')}/>
    
             <Image  style={styles.akisha} source={require('../assets/akisha.png')}/>
           
           </View> */}
-
-          </LinearGradient>
-       
-      </View>
-
+      </LinearGradient>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-
-  
   welcome: {
     marginTop: -18,
-    marginLeft: win.width/16,
+    marginLeft: win.width / 16,
     marginBottom: 15,
     fontSize: hp('4.1%'),
     fontFamily: 'Cambay-Bold',
     color: '#ab0e0e',
-    textShadowColor: "white",
+    textShadowColor: 'white',
     textShadowOffset: {
-    width: 3,
-    height: 1,
+      width: 3,
+      height: 1,
     },
     textShadowRadius: 20,
     position: 'absolute',
   },
 
-  unameText:{
+  unameText: {
     marginTop: 50,
     borderColor: 'black',
     fontSize: hp('2.3%'),
@@ -104,7 +161,6 @@ const styles = StyleSheet.create({
     padding: 5,
     marginBottom: 10,
     position: 'absolute',
-    
   },
 
   passText: {
@@ -124,19 +180,18 @@ const styles = StyleSheet.create({
     padding: 7,
     marginBottom: 10,
     position: 'absolute',
-    
   },
-  
+
   login: {
     marginTop: 215,
     height: 35,
     width: 290,
     borderRadius: 20,
     backgroundColor: '#ab0e0e',
-    shadowColor: "black",
+    shadowColor: 'black',
     shadowOffset: {
-    width: 3,
-    height: 2,
+      width: 3,
+      height: 2,
     },
     shadowOpacity: 10,
     shadowRadius: 200,
@@ -148,7 +203,7 @@ const styles = StyleSheet.create({
     marginTop: 5,
     fontSize: hp('2.4%'),
     fontFamily: 'Poppins-Medium',
-    alignSelf: 'center',    
+    alignSelf: 'center',
     position: 'absolute',
   },
 
@@ -164,19 +219,19 @@ const styles = StyleSheet.create({
 
   iconsContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',  
-    marginTop: '10%',  
-},
+    justifyContent: 'space-between',
+    marginTop: '10%',
+  },
 
   // avatars: {
   //   marginTop: '72%',
   //   flexDirection: 'row',
-  //   alignContent: "space-between", 
+  //   alignContent: "space-between",
   //   marginBottom: '0%',
   //   position: 'absolute',
   // },
 
-  iicsboy:{
+  iicsboy: {
     marginTop: '-13%',
     height: screenheight / 4,
     width: screenwidth / 2.7,
@@ -184,7 +239,7 @@ const styles = StyleSheet.create({
     // transform: [{ rotate: '0deg' }],
   },
 
-  akisha:{
+  akisha: {
     marginTop: '-8.5%',
     marginRight: '-2%',
     height: screenheight / 4.6,
@@ -192,7 +247,6 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
     // transform: [{ rotate: '0deg' }],
   },
-
 
   // loadingBg: {
   //   height: 600,
@@ -202,9 +256,6 @@ const styles = StyleSheet.create({
   //   opacity: 0.3, //0.7
   //   position: 'absolute',
   // },
-
- 
-
 });
 
 export default Login;
