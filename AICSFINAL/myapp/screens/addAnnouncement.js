a48e7840dea5e18d399acc631efe52c56977994d
rainbowcakepat <60645849+rainbowcakepat@users.noreply.github.com>
2021-08-31 22:06:21 +0800

    Open Camera, Upload Image no json
    ***Henlo, pakisunod-sunod eto lahat:
    
    ***Buksan ang package json, check sa dependencies kung ang mga firebase/app and firebase/firestore ay 12.7.2. TAPOS IF OKAY, GOODS YAN
    
    ***Install niyo ito:
    
    npm i react-native-image-crop-picker
    
    yarn add @react-native-firebase/storage
    
    npm outdated
    
    ***ISANG LINE TO:
    
    npm install @react-native-firebase/app@latest @react-native-firebase/storage@latest @react-native-firebase/firestore@latest

--- AICSFINAL/myapp/screens/addAnnouncement.js
+++ AICSFINAL/myapp/screens/addAnnouncement.js
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, Text, TextInput, View, TouchableOpacity, ScrollView } from 'react-native';
+import { Alert, Text, TextInput, View, TouchableOpacity, Image, Platform } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import { db } from '../firebase';
+import ImagePicker from 'react-native-image-crop-picker';
+import storage from '@react-native-firebase/storage';
 
+//Try
 const AddAnnouncement = ({navigation}) => {
 
   const time =  new Date().getDate();
   const [titles, setTitle] = useState('');
   const [links, setLink] = useState('');
   const [loader, setLoading] = useState(false);
+  
+ 
+  const [photo, setPhoto] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [transferred, setTransferred] = useState(0);
 
   //Add Announcements:
   const addAnnouncementNow = async() => {
@@ -31,6 +39,44 @@ const AddAnnouncement = ({navigation}) => {
     });
   }
 
+  const choosePhotoFromImageLibrary = () => {
+    ImagePicker.openPicker({
+      width: 800,
+      height: 1200,
+      cropping: true,
+    }).then((photo) => {
+      console.log(photo);
+      const imageUri = Platform.OS == 'ios' ? photo.sourceURL : photo.path;
+      setPhoto(imageUri);
+      Alert.alert('You have attached an image successfully', imageUri);
+    })
+    .catch((error) => {
+      Alert.alert('No image attached');
+      console.log('Error:', error);
+    });
+  }
+
+
+  const uploadPhoto = async () => {
+    const uploadUri = photo;
+    let filename = uploadUri.substring(uploadUri.lastIndexOf('/') + 1);
+
+    setUploading(true);
+    try {
+      await storage().ref(filename).putFile(uploadUri);
+
+      setUploading(false);
+      Alert.alert('Image uploaded to Firebase Cloud');
+    }
+    catch(e){
+      Alert.alert('No image attached');
+      console.log(e);
+    }
+
+    setPhoto(null);
+  }
+
+  
  
   return (
     <View>
@@ -44,6 +90,24 @@ const AddAnnouncement = ({navigation}) => {
       <TouchableOpacity style={{ width: 300, height: 20, backgroundColor: loader ? 'gray' : 'purple'}} onPress={addAnnouncementNow} >
         <Text>submit</Text>
       </TouchableOpacity>
+      <TouchableOpacity style={{ width: 300, height: 20, backgroundColor: loader ? 'gray' : 'purple'}} onPress={choosePhotoFromImageLibrary} >
+        <Text>photo</Text>
+      </TouchableOpacity>
+      <View>
+        {photo == null ? (
+          null
+          ) : 
+          <View>
+          <Image source={{ uri: photo }}></Image>
+          <TouchableOpacity style={{ width: 300, height: 20, backgroundColor: loader ? 'gray' : 'purple'}} onPress={uploadPhoto} >
+            <Text>photo post</Text>
+          </TouchableOpacity>
+          </View>}
+       
+      </View>
+        
+
+      
     </View>
   )
 }
